9ae2a795fcb3ca2acccae345357583ea1edcdb03
Abhishek rana <me.abhishekrana0317@gmail.com>
2024-09-24 10:31:54 +0530

    added

--- main.tf
+++ main.tf
@@ -78,11 +78,3 @@ resource "azurerm_linux_virtual_machine" "example" {
   # Optional: Additional configuration (e.g., tags)
 }
 
-# Outputs
-output "resource_group_name" {
-  value = azurerm_resource_group.example.name
-}
-
-output "vm_public_ip" {
-  value = azurerm_network_interface.nic.ip_configuration[0].private_ip_address
-}
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,8 @@
-
-# Outputs
+# outputs.tf
 output "resource_group_name" {
- value = azurerm_resource_group.example.name  # Correct the value to reference the resource group
+ value = azurerm_resource_group.example.name
 }
 
 output "vm_public_ip" {
- value = azurerm_network_interface.nic.ip_configuration[0].private_ip_address  # This line is fine if you're accessing the correct resource
+ value = azurerm_network_interface.nic.ip_configuration[0].private_ip_address
 }
