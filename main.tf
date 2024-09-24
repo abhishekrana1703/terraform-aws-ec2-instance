bcdc180bfdb19bf39889e4bb396c8acfe5e0eaa8
Abhishek rana <me.abhishekrana0317@gmail.com>
2024-09-24 10:18:47 +0530

    added

--- main.tf
+++ main.tf
@@ -1,49 +1,88 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"  # Specify the appropriate version of the Azure provider
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
+
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "main" {
+# Resource Group
+resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_virtual_machine" "vm" {
-  name                  = var.vm_name
-  location              = azurerm_resource_group.main.location
-  resource_group_name   = azurerm_resource_group.main.name
-  vm_size              = var.vm_size
-
-  storage_os_disk {
-    name              = "${var.vm_name}-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-  }
-
-  os_profile {
-    computer_name  = var.vm_name
-    admin_username = var.admin_username
-    admin_password = var.admin_password
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  network_interface_ids = [azurerm_network_interface.nic.id]
-
-  # Define additional configurations like network interfaces, etc.
+# Virtual Network
+resource "azurerm_virtual_network" "example" {
+  name                = "${var.resource_group_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
+# Subnet
+resource "azurerm_subnet" "example" {
+  name                 = "${var.resource_group_name}-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Network Interface
 resource "azurerm_network_interface" "nic" {
   name                = "${var.vm_name}-nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
-    name                          = "${var.vm_name}-ipconfig"
-    subnet_id                     = azurerm_subnet.main.id
+    name                          = "internal"
+    subnet_id                    = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-# Add your virtual network and subnet resources here if needed
+# Virtual Machine
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_DS1_v2"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  # Source image configuration
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  # OS Disk configuration
+  os_disk {
+    caching       = "ReadWrite"
+    create_option = "FromImage"
+  }
+
+  # Optional: Additional configuration (e.g., tags)
+}
+
+# Outputs
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
+}
+
+output "vm_public_ip" {
+  value = azurerm_network_interface.nic.ip_configuration[0].private_ip_address
+}
