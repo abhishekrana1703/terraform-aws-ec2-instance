718251ce5491d1477082d06eb7b04d1807d7dee3
Abhishek rana <me.abhishekrana0317@gmail.com>
2024-09-24 10:29:03 +0530

    added

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,9 @@
+
 # Outputs
 output "resource_group_name" {
-  value = example-resources//
+ value = azurerm_resource_group.example.name  # Correct the value to reference the resource group
 }
 
 output "vm_public_ip" {
-  value = azurerm_network_interface.nic.ip_configuration[0].private_ip_address
+ value = azurerm_network_interface.nic.ip_configuration[0].private_ip_address  # This line is fine if you're accessing the correct resource
 }
