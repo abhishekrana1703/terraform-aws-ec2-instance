3f0e3a483e93c0c43a5e0c51de990a9bc2dda964
Abhishek Rana <me.abhishekrana0317@gmail.com>
2024-09-26 09:40:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,39 @@
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
+variable "region" {
+  description = "AWS region where the instance will be created."
+  default     = "us-east-1"
 }
 
-variable "location" {
-  description = "The Azure location for the resources"
-  type        = string
+variable "profile" {
+  description = "AWS CLI profile name to use."
+  default     = "default"
 }
 
-variable "vm_name" {
-  description = "The name of the virtual machine"
-  type        = string
+variable "ami" {
+  description = "The AMI ID to use for the instance."
+  default     = "ami-0e86e20dae9224db8"  # Update with your desired AMI ID
 }
 
-variable "admin_username" {
-  description = "The administrator username for the virtual machine"
-  type        = string
+variable "instance_type" {
+  description = "The instance type (e.g., m5.large)."
+  default     = "m5.large"
 }
 
-variable "admin_password" {
-  description = "The administrator password for the virtual machine"
-  type        = string
+variable "availability_zone" {
+  description = "The availability zone (e.g., us-east-1a)."
+  default     = "us-east-1a"  # You can choose an AZ in your region
+}
+
+variable "key_name" {
+  description = "The name of the SSH key to use."
+  default     = "key"  # Change to your key pair name
+}
+
+variable "volume_size" {
+  description = "The size of the root volume in GB."
+  default     = 30
+}
+
+variable "server_name" {
+  description = "The name to assign to the instance."
+  default     = "my-instance"
 }
